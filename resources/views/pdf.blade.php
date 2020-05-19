ab94863be85db971987746aa0b03db11826f0032
Kaeland Chatman <kaeland1@gmail.com>
2020-05-18 22:34:55 -0500

    Checking if blade template headers change styling.

--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -1,28 +1,40 @@
-@extends('layout')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" />
+    <title>LarvelPDF</title>
+</head>
+<body>
+    <div class="container">
+        <div class="content">
+            <table class="table is-bordered is-striped">
+                <thead>
+                    <tr>
+                        <td><b>Show Name</b></td>
+                        <td><b>Series</b></td>
+                        <td><b>Lead Actor</b></td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            {{$show->show_name}}
+                        </td>
+                        <td>
+                            {{$show->series}}
+                        </td>
+                        <td>
+                            {{$show->lead_actor}}
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <div class="button is-link">Click Me</div>
+        </div>
+    </div>
+</body>
+</html>
 
-@section('content')
-<div class="container">
-    <table class="table is-bordered is-striped">
-        <thead>
-            <tr>
-                <td><b>Show Name</b></td>
-                <td><b>Series</b></td>
-                <td><b>Lead Actor</b></td>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>
-                    {{$show->show_name}}
-                </td>
-                <td>
-                    {{$show->series}}
-                </td>
-                <td>
-                    {{$show->lead_actor}}
-                </td>
-            </tr>
-        </tbody>
-    </table>
-</div>
-@endsection
